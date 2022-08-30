f1942899db199c4c6319db6ac67a911dfb6419c3
hxshan <galgodageheshan@gmail.com>
2022-08-30 20:03:55 +0530

    fw chane

--- WFM.DAL/STWFMModel1.Designer.cs
+++ WFM.DAL/STWFMModel1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\SIGN-TECH\Src\ST_WFM\WFM.DAL\STWFMModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\project\ST_WFM\WFM.DAL\STWFMModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
